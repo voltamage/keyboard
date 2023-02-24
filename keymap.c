b0a6bec8447aafe0ef2eaee9c22c761ba20741db
voltamage <37029882+voltamage@users.noreply.github.com>
2023-02-24 00:40:08 -0800

    standardized outer edge keys

--- keymap.c
+++ keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * | LCtr |   Z  |   X  |   C  |   D  |   V  |-------|    |-------|   K  |   H  |   ,  |   .  |   /  |  \   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LAlt | LGui |  Esc |Space | / Lower /       \Raise \  |Enter |   :  | RGui | RAlt |
+ *            | LAlt | LGui |  Esc |Space | / Lower /       \Raise \  |Enter |   :  | RGui | Menu |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,                         KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_BSPC,
   KC_LSFT,  KC_A,     KC_R,     KC_S,     KC_T,     KC_G,                         KC_M,     KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,
   KC_LCTL,  KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,     _______,  _______,  KC_K,     KC_H,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_BSLS,
-                      KC_LALT,  KC_LGUI,  KC_ESC,   KC_SPC,MO(_LOWER), MO(_RAISE),KC_ENT,   KC_COLN,  KC_RGUI,  KC_RALT
+                      KC_LALT,  KC_LGUI,  KC_ESC,   KC_SPC,MO(_LOWER), MO(_RAISE),KC_ENT,   KC_COLN,  KC_RGUI,  KC_APP
   ),
 
 /*
